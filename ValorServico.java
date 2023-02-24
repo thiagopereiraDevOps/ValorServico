551aaa3222019d253d550d09296e856fdacd010f
Thiago Pereira Da Silva <thiago.silva@venturus.org.br>
2023-02-24 18:15:37 -0300

    Meus arquivos de valorservico

---
+++ ValorServico.java
@@ -0,0 +1,28 @@
+package  exercicios;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class ValorServico {
+
+    public static void main(String[] args) {
+        //variáveis
+        double hora,remuneracao,custo,cargaHoraria;
+        // objetos
+        Scanner teclado = new Scanner(System.in);
+        DecimalFormat formatador = new DecimalFormat("#0.00");
+        // entrada
+        System.out.println("Cálculo do valor da hora de um srrviço");
+        System.out.print("Remuneração mensal pretendida: ");
+        remuneracao = teclado.nextDouble();
+        System.out.print("Custo operacional mensal: ");
+        custo = teclado.nextDouble();
+        System.out.print("Carga horária mensal de trabalho: ");
+        cargaHoraria = teclado.nextDouble();
+        // processamento
+        hora = (remuneracao + (remuneracao * 0.3) + custo + (remuneracao) * 0.2)) / cargaHoraria;
+        // saída
+        System.out.println("Valor da hora : " + formatador.format(hora));
+        teclado.close();
+    }
+}
